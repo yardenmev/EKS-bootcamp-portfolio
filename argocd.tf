fcf8529cfd89ef1a2b13cf64297c355d3646a148
yarenmev <hrsink@gmail.com>
2023-03-18 11:35:48 +0000

    resource

--- argocd.tf
+++ argocd.tf
@@ -21,37 +21,37 @@ resource "helm_release" "argocd" {
  
 }
 
-# resource "null_resource" "argo_password" {
-#   depends_on = [
-#     helm_release.argocd
-#   ]
-#   provisioner "local-exec" {
-#     command = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d && echo"
-#     when    = create
-#   }
-# }
+resource "null_resource" "argo_password" {
+  depends_on = [
+    helm_release.argocd
+  ]
+  provisioner "local-exec" {
+    command = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d && echo"
+    when    = create
+  }
+}
 
-# provider "argocd" {
-#   server_addr = "argocd.local:443"
-#   api_version = "client.authentication.k8s.io/v1beta1"
-# }
+provider "argocd" {
+  server_addr = "argocd.local:443"
+  api_version = "client.authentication.k8s.io/v1beta1"
+}
 
-# resource "argocd_application" "todo" {
-#   metadata {
-#     name      = "todo-app"
-#     namespace = "default" 
-#   }
-#   spec {
-#     project = "myproject"
-#     source {
-#     repo_url = "https://github.com/yardenmev/GitOps-bootcamp-portfolio"
-#     path            = "todo-chart"
-#     target_revision = "HEAD"
-#     }
-#   }
-#   destination {
-#     server    = "https://kubernetes.default.svc"
-#     namespace = "default"
-#   }
-# }
+resource "argocd_application" "todo" {
+  metadata {
+    name      = "todo-app"
+    namespace = "default" 
+  }
+  spec {
+    project = "myproject"
+    source {
+    repo_url = "https://github.com/yardenmev/GitOps-bootcamp-portfolio"
+    path            = "todo-chart"
+    target_revision = "HEAD"
+    }
+  }
+  destination {
+    server    = "https://kubernetes.default.svc"
+    namespace = "default"
+  }
+}
 
