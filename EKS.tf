4e335be4710fbfd90f9cd7d4049fa810a1c84fa2
yarenmev <hrsink@gmail.com>
2023-03-19 13:15:04 +0000

    eci

--- EKS.tf
+++ EKS.tf
@@ -49,3 +49,17 @@ module "eks" {
   }
 
 }
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.14"
+
+  role_name = "yarden_csi"
+  attach_ebs_csi_policy = true
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
